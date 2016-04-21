618e2be7a7a9016d27116244ee22f277cfd57fb0
DGovorov <heavenbox@mail.ru>
2016-04-21 16:44:04 +0300

    FileGenerator

---
+++ src/homework10/FileGenerator.java
@@ -0,0 +1,58 @@
+package homework10;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by Dim on 21.04.2016.
+ */
+public class FileGenerator {
+
+    public void generateFile(String fileName, int kilobytes) {
+        File file = new File(fileName);
+        if (!file.exists()){
+            try {
+                Files.createFile(Paths.get(fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (DataOutputStream output = new DataOutputStream(new FileOutputStream(fileName, true))) {
+            int iterations = kilobytes*256;
+            for (int i = 0; i < iterations; i++) {
+                output.writeInt(Integer.MAX_VALUE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generateFileFaster(String fileName, int kilobytes){
+        File file = new File(fileName);
+        if (!file.exists()){
+            try {
+                Files.createFile(Paths.get(fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true), 4)) {
+            int iterations = kilobytes*1024/3; //awful workaround
+            for (int i = 0; i < iterations; i++) {
+                writer.write(Integer.MAX_VALUE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        FileGenerator generator = new FileGenerator();
+        generator.generateFile("firstFile.dat", 5000);
+        generator.generateFileFaster("secondFile.dat", 5000);
+    }
+}
